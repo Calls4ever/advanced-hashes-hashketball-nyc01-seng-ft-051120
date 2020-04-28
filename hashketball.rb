de14a6c9277747475b59792e83360232d1a2e848
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-28 22:09:28 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,7 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
-  {
+  data={
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
@@ -124,6 +125,215 @@ def game_hash
       ]
     }
   }
+  return data
+end
+def num_points_scored(name)
+  points_scored=0
+  data= game_hash
+data[:home][:players].each{|n|
+   if name == n[:player_name]
+     points_scored=n[:points]
+   end
+   }
+if points_scored==0
+  data[:away][:players].each{|n|
+     if name == n[:player_name]
+       points_scored=n[:points]
+     end
+     }
+end
+if points_scored==0
+  return "fuck you China! Thanks for corona!"
+else return points_scored
+end
+
+end
+def shoe_size(name)
+  size=3000
+  data= game_hash
+data[:home][:players].each{|n|
+   if name == n[:player_name]
+     size=n[:shoe]
+   end
+   }
+if size==3000
+  data[:away][:players].each{|n|
+     if name == n[:player_name]
+       size=n[:shoe]
+     end
+     }
+end
+if size==3000
+  return "i love you 3000 #{name}!"
+else return size
+end
+
+end
+def team_colors(team)
+  data=game_hash
+  colors=""
+  if data[:home][:team_name]==team
+    colors=[data[:home][:colors][0] ,data[:home][:colors][1]]
+elsif data[:away][:team_name]==team
+    colors=[data[:away][:colors][0] , data[:away][:colors][1]]
+  else colors="Team is not playing in this matach!"
+    return colors
+end
 end
 
 # Write code here
+def team_names
+  data=game_hash
+return [ data[:home][:team_name], data[:away][:team_name]]
+
+end
+
+def player_numbers(name="Brooklyn Nets")
+data =game_hash
+number=[]
+
+data[:home][:players].each{ |n|
+if name == data[:home][:team_name]
+  number.push(n[:number])
+end
+}
+
+
+  data[:away][:players].each{ |n|
+    if name == data[:away][:team_name]
+      number.push(n[:number])
+end
+}
+if number==[]
+  number=["Tibet lost it's country to China in #{number}!"]
+else  return number.sort
+end
+
+end
+def player_stats(name="Alan Anderson")
+  data=game_hash
+  stats={}
+  data[:home][:players].each{ |key|
+    if key[:player_name]==name
+      key.each{ |n, m|
+        stats[n]=m
+    }
+end
+  }
+  data[:away][:players].each{ |key|
+    if key[:player_name]==name
+    key.each{ |n, m|
+        stats[n]=m
+    }
+end
+  }
+return stats
+end
+def big_shoe_rebounds
+  data=game_hash
+  biggest_shoe=0
+  rebounds=0
+  data[:home][:players].each{|n|
+    if n[:shoe]>biggest_shoe
+      biggest_shoe=n[:shoe]
+      rebounds=n[:rebounds]
+    end
+  }
+  data[:away][:players].each{|n|
+    if n[:shoe]>biggest_shoe
+      biggest_shoe=n[:shoe]
+      rebounds=n[:rebounds]
+    end
+  }
+return rebounds
+end
+def most_points_scored
+  data=game_hash
+  highest_points=0
+  high_scorer=""
+  data[:home][:players].each{ |n|
+    if n[:points]>highest_points
+      highest_points=n[:points]
+      high_scorer=n[:player_name]
+    end
+  }
+  data[:away][:players].each{ |n|
+    if n[:points]>highest_points
+      highest_points=n[:points]
+      high_scorer=n[:player_name]
+    end
+  }
+  return high_scorer
+
+end
+def winning_team
+  data=game_hash
+  home=0
+  away=0
+  data[:home][:players].each{ |n|
+    home +=n[:points]
+  }
+  data[:away][:players].each{ |n|
+    away +=n[:points]
+  }
+  if away>home
+     return "WOW!! Charlotte Hornets won the game!!1"
+     else
+    return  "WOWOWOWOW!!! Brooklyn Nets won the game!!"
+     end
+
+end
+
+
+def player_with_longest_name
+  data=game_hash
+  name_length=0
+  name=""
+  data[:home][:players].each{ |n|
+    if n[:player_name].length>name_length
+      name_length=n[:player_name].length
+      name=n[:player_name]
+    end
+  }
+  data[:away][:players].each{ |n|
+    if n[:player_name].length>name_length
+      name_length=n[:player_name].length
+      name=n[:player_name]
+    end
+  }
+  return name
+  binding.pry
+end
+def long_name_steals_a_ton?
+  data=game_hash
+  steal=0
+  longest_name=0
+  stealer=""
+  long_name=""
+  data[:home][:players].each{ |n|
+    if n[:player_name].length>longest_name
+      longest_name=n[:player_name].length
+      long_name=n[:player_name]
+    end
+    if n[:steals]>steal
+      steal=n[:steals]
+      stealer=n[:player_name]
+    end
+  }
+  data[:away][:players].each{ |n|
+    if n[:player_name].length>longest_name
+      longest_name=n[:player_name].length
+      long_name=n[:player_name]
+    end
+    if n[:steals]>steal
+      steal=n[:steals]
+      stealer=n[:player_name]
+    end
+  }
+if stealer==long_name
+  return true
+else false
+end
+binding.pry
+end
+binding.pry
